e5f238adb25cc44d95f3b77a2af4303c42b8a943
galwort <wthomasgorbett@gmail.com>
2024-01-01 21:38:17 -0500

    Implement tracking form

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -9,10 +9,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideFirestore(() => getFirestore())],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    ReactiveFormsModule,
+  ],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- ui/src/app/home/home.page.ts
+++ ui/src/app/home/home.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { environment } from 'src/environments/environment';
+import { FormControl, FormGroup } from '@angular/forms';
+
+export const app = initializeApp(environment.firebase);
+export const db = getFirestore(app);
 
 @Component({
   selector: 'app-home',
@@ -6,7 +13,37 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  trackerForm = new FormGroup({
+    date: new FormControl(''),
+    time: new FormControl(''),
+    activity: new FormControl('')
+  });
 
   constructor() {}
 
+  async submitData() {
+    const formData = this.trackerForm.value;
+    const date = formData.date ?? '';
+    const time = formData.time?.replace(/:/g, '') ?? '';
+    const activity = formData.activity ?? '';
+  
+    if (!date || !time) {
+      console.error("Date and time are required.");
+      return;
+    }
+  
+    const docId = date.replace(/-/g, '') + time;
+    const docRef = doc(db, 'tracker', docId);
+  
+    try {
+      await setDoc(docRef, {
+        activity: activity,
+        timestamp: new Date(date + 'T' + time)
+      });
+      console.log('Document written with ID: ', docId);
+      this.trackerForm.reset();
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
 }
