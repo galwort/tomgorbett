5f33ddd8adc7ec099b185a2163077a4a15c30885
galwort <wthomasgorbett@gmail.com>
2024-01-05 08:29:10 -0500

    Direct enter on password to timetracker page

--- ui/src/app/pages/unlock/unlock.page.ts
+++ ui/src/app/pages/unlock/unlock.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-unlock',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./unlock.page.scss'],
 })
 export class UnlockPage implements OnInit {
+  password: string = '';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  checkPassword() {
+    const correctPassword = 'BattyFives';
+    if (this.password === correctPassword) {
+      this.router.navigate(['/timetracker']);
+    } else {
+      console.log('Incorrect password');
+    }
+  }
 }
