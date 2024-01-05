b13fe8673beac86b9826399421d54afadcaca883
galwort <wthomasgorbett@gmail.com>
2024-01-05 09:12:32 -0500

    Set up service and interface for firebase login

---
+++ ui/src/app/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+
+import { Injectable } from '@angular/core';
+import { LoginData } from '../interfaces/login-data.interface';
+
+@Injectable({
+providedIn: 'root',
+})
+export class AuthService {
+
+    constructor(private auth: Auth) {}
+
+    login({ email, password }: LoginData) {
+        return signInWithEmailAndPassword(this.auth, email, password);
+    }
+}
