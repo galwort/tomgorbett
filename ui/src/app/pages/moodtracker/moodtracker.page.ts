d558af0060a20f4da06ec92df352c2146a2aada3
galwort <wthomasgorbett@gmail.com>
2024-01-07 18:38:21 -0500

    Implement list and changing icon functionality

--- ui/src/app/pages/moodtracker/moodtracker.page.ts
+++ ui/src/app/pages/moodtracker/moodtracker.page.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./moodtracker.page.scss'],
 })
 export class MoodtrackerPage implements OnInit {
+  icons = ['skull', 'sad', 'ellipse', 'happy', 'heart-circle'];
+  dietIndex = 2;
+  marriageIndex = 2;
+  parentsIndex = 2;
+  productivityIndex = 2;
+  sleepIndex = 2;
+
+  changeDietIcon() {
+    this.dietIndex = (this.dietIndex + 1) % this.icons.length;
+  }
+
+  changeMarriageIcon() {
+    this.marriageIndex = (this.marriageIndex + 1) % this.icons.length;
+  }
+
+  changeParentsIcon() {
+    this.parentsIndex = (this.parentsIndex + 1) % this.icons.length;
+  }
+
+  changeProductivityIcon() {
+    this.productivityIndex = (this.productivityIndex + 1) % this.icons.length;
+  }
+
+  changeSleepIcon() {
+    this.sleepIndex = (this.sleepIndex + 1) % this.icons.length;
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
