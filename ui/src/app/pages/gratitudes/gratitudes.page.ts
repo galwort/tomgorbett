92127a4c77b0b3c7a85e9df5fad5df41e8d34f74
galwort <wthomasgorbett@gmail.com>
2024-01-09 08:15:51 -0500

    Submit gratitude to firestore

--- ui/src/app/pages/gratitudes/gratitudes.page.ts
+++ ui/src/app/pages/gratitudes/gratitudes.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
+import { environment } from 'src/environments/environment';
+
+export const app = initializeApp(environment.firebase);
+export const db = getFirestore(app);
 
 @Component({
   selector: 'app-gratitudes',
@@ -6,10 +12,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gratitudes.page.scss'],
 })
 export class GratitudesPage implements OnInit {
+  gratitude: string = '';
+  isSaving: boolean = false;
+  saveButtonText: string = 'Save';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  private getFormattedDate(): string {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}${month}${day}`;
+  }
+
+  async submit() {
+    this.isSaving = true;
+    this.saveButtonText = 'Saving...';
+  
+    const dateId = this.getFormattedDate();
+    try {
+      const docRef = await setDoc(doc(db, 'gratitudes', dateId), { gratitude: this.gratitude });
+      this.saveButtonText = 'Saved';
+    } catch (error) {
+      console.error('Error saving: ', error);
+      this.saveButtonText = 'Save Failed';
+    } finally {
+      this.isSaving = false;
+      setTimeout(() => this.saveButtonText = 'Save', 2000);
+    }
+  }  
 }
