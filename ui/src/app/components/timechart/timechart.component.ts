0f884d254c70976d9bdd758ac13ba45484f8af8d
galwort <wthomasgorbett@gmail.com>
2024-01-24 16:21:10 -0500

    Create chart with bogus data

--- ui/src/app/components/timechart/timechart.component.ts
+++ ui/src/app/components/timechart/timechart.component.ts
@@ -1,14 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-timechart',
   templateUrl: './timechart.component.html',
   styleUrls: ['./timechart.component.scss'],
 })
-export class TimechartComponent  implements OnInit {
+export class TimechartComponent implements OnInit {
+  chart: any;
 
-  constructor() { }
+  constructor() { 
+    Chart.register(...registerables);
+  }
 
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.chart = new Chart('myChart', {
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+          label: 'Sample Data',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 }
