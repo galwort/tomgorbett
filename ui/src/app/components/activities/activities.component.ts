47b6ef39fdd4d9a1b5d398f5030f66b5bb0ad14a
galwort <wthomasgorbett@gmail.com>
2024-04-30 20:01:53 -0400

    Pull activities from firestore

--- ui/src/app/components/activities/activities.component.ts
+++ ui/src/app/components/activities/activities.component.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  setDoc,
+} from 'firebase/firestore';
+import { environment } from 'src/environments/environment';
+
+export const app = initializeApp(environment.firebase);
+export const db = getFirestore(app);
 
 @Component({
   selector: 'app-activities',
   templateUrl: './activities.component.html',
   styleUrls: ['./activities.component.scss'],
 })
-export class ActivitiesComponent  implements OnInit {
+export class ActivitiesComponent implements OnInit {
+  activityForm = new FormGroup({
+    name: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchActivities();
+  }
 
+  async fetchActivities() {
+    const querySnapshot = await getDocs(collection(db, 'activities'));
+  }
 }
