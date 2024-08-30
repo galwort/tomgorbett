6cc522cb92f0787674e0a3290b071649fa0a21e0
galwort <wthomasgorbett@gmail.com>
2024-08-30 11:33:06 -0400

    Being time line chart

--- ui/src/app/components/timechart/timechart.component.ts
+++ ui/src/app/components/timechart/timechart.component.ts
@@ -11,6 +11,8 @@ interface DailyData {
   screenTime: number;
   work: number;
   productive: number;
+  sleep: number;
+  other: number;
 }
 
 interface ChartData {
@@ -18,6 +20,8 @@ interface ChartData {
   screenTimeData: number[];
   workData: number[];
   productiveData: number[];
+  sleepData: number[];
+  otherData: number[];
 }
 
 @Component({
@@ -34,7 +38,9 @@ export class TimechartComponent implements OnInit {
   }
 
   async ngOnInit() {
+    console.log('Component initialized. Fetching chart data...');
     const chartData = await this.fetchChartData();
+    console.log('Chart data fetched:', chartData);
     this.initializeChart(chartData);
   }
 
@@ -56,11 +62,22 @@ export class TimechartComponent implements OnInit {
       .toString()
       .padStart(2, '0')}${startDate.getDate().toString().padStart(2, '0')}`;
 
+    console.log('Default date range:', {
+      startDate: startDateString,
+      endDate: endDate,
+    });
     return { startDate: startDateString, endDate: endDate };
   }
 
   async fetchChartData(): Promise<ChartData> {
     const { startDate, endDate } = this.getDefaultDateRange();
+
+    console.log(
+      'Fetching data from Firestore between',
+      startDate,
+      'and',
+      endDate
+    );
 
     const activitiesQuery = query(collection(this.db, 'activities'));
     const activitiesSnapshot = await getDocs(activitiesQuery);
@@ -69,10 +86,12 @@ export class TimechartComponent implements OnInit {
       activitiesMap.set(doc.id, doc.data());
     });
 
+    console.log('Activities data fetched:', activitiesMap);
+
     const trackerQuery = query(collection(this.db, 'tracker'));
     const trackerSnapshot = await getDocs(trackerQuery);
 
-    let dailyData: Record<string, DailyData> = {};
+    let weeklyData: Record<string, DailyData> = {};
 
     trackerSnapshot.docs.forEach((trackerDoc) => {
       const docId = trackerDoc.id;
@@ -83,71 +102,109 @@ export class TimechartComponent implements OnInit {
         if (data['Activity']) {
           const activityData = activitiesMap.get(data['Activity']);
           if (activityData) {
-            const month = parseInt(docDate.substring(4, 6), 10);
-            const day = parseInt(docDate.substring(6, 8), 10);
-            const formattedDate = `${month}/${day}`;
+            const weekNumber = this.getWeekNumber(
+              new Date(
+                parseInt(docDate.substring(0, 4)),
+                parseInt(docDate.substring(4, 6)) - 1,
+                parseInt(docDate.substring(6, 8))
+              )
+            );
 
-            if (!dailyData[formattedDate]) {
-              dailyData[formattedDate] = {
+            if (!weeklyData[weekNumber]) {
+              weeklyData[weekNumber] = {
                 screenTime: 0,
                 work: 0,
                 productive: 0,
+                sleep: 0,
+                other: 0,
               };
             }
 
-            dailyData[formattedDate].screenTime += activityData['Screen_Time']
+            weeklyData[weekNumber].screenTime += activityData['Screen_Time']
               ? activityData['Screen_Time'] / 4
               : 0;
-            dailyData[formattedDate].work += activityData['Work']
+            weeklyData[weekNumber].work += activityData['Work']
               ? activityData['Work'] / 4
               : 0;
-            dailyData[formattedDate].productive += activityData['Productive']
+            weeklyData[weekNumber].productive += activityData['Productive']
               ? activityData['Productive'] / 4
               : 0;
+            weeklyData[weekNumber].sleep += activityData['Sleep']
+              ? activityData['Sleep'] / 4
+              : 0;
+            weeklyData[weekNumber].other += activityData['Other']
+              ? activityData['Other'] / 4
+              : 0;
           }
-        } else {
-          console.log(
-            `Activity is undefined for trackerDoc ID: ${trackerDoc.id}`
-          );
         }
       }
     });
 
+    console.log('Weekly data processed:', weeklyData);
+
     let chartData: ChartData = {
-      labels: Object.keys(dailyData).sort(
-        (a, b) => new Date(a).getTime() - new Date(b).getTime()
-      ),
-      screenTimeData: Object.values(dailyData).map((d) => d.screenTime),
-      workData: Object.values(dailyData).map((d) => d.work),
-      productiveData: Object.values(dailyData).map((d) => d.productive),
+      labels: Object.keys(weeklyData).sort(),
+      screenTimeData: Object.values(weeklyData).map((d) => d.screenTime),
+      workData: Object.values(weeklyData).map((d) => d.work),
+      productiveData: Object.values(weeklyData).map((d) => d.productive),
+      sleepData: Object.values(weeklyData).map((d) => d.sleep),
+      otherData: Object.values(weeklyData).map((d) => d.other),
     };
+
+    console.log('Final chart data prepared:', chartData);
 
     return chartData;
   }
 
   initializeChart(chartData: ChartData) {
-    this.chart = new Chart('myChart', {
+    console.log('Initializing chart with data:', chartData);
+    const canvas = document.getElementById('myChart') as HTMLCanvasElement;
+    console.log('Canvas element:', canvas);
+
+    if (!canvas) {
+      console.error('Canvas element not found');
+      return;
+    }
+
+    this.chart = new Chart(canvas, {
       type: 'line',
       data: {
         labels: chartData.labels,
         datasets: [
           {
             label: 'Screen Time',
-            backgroundColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
             borderColor: 'rgb(255, 99, 132)',
             data: chartData.screenTimeData,
+            fill: true,
           },
           {
             label: 'Work',
-            backgroundColor: 'rgb(54, 162, 235)',
+            backgroundColor: 'rgba(54, 162, 235, 0.5)',
             borderColor: 'rgb(54, 162, 235)',
             data: chartData.workData,
+            fill: true,
           },
           {
             label: 'Productive',
-            backgroundColor: 'rgb(75, 192, 192)',
+            backgroundColor: 'rgba(75, 192, 192, 0.5)',
             borderColor: 'rgb(75, 192, 192)',
             data: chartData.productiveData,
+            fill: true,
+          },
+          {
+            label: 'Sleep',
+            backgroundColor: 'rgba(153, 102, 255, 0.5)',
+            borderColor: 'rgb(153, 102, 255)',
+            data: chartData.sleepData,
+            fill: true,
+          },
+          {
+            label: 'Other',
+            backgroundColor: 'rgba(255, 159, 64, 0.5)',
+            borderColor: 'rgb(255, 159, 64)',
+            data: chartData.otherData,
+            fill: true,
           },
         ],
       },
@@ -155,6 +212,7 @@ export class TimechartComponent implements OnInit {
         scales: {
           y: {
             beginAtZero: true,
+            stacked: true,
             title: {
               display: true,
               text: 'Hours',
@@ -176,5 +234,16 @@ export class TimechartComponent implements OnInit {
         },
       },
     });
+
+    console.log('Chart initialized:', this.chart);
+  }
+
+  getWeekNumber(date: Date): string {
+    const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+    const pastDaysOfYear =
+      (date.getTime() - firstDayOfYear.getTime()) / 86400000;
+    return `${date.getFullYear()}-W${Math.ceil(
+      (pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7
+    )}`;
   }
 }
