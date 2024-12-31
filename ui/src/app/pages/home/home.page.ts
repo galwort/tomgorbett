cd2120ac9260bbbd565a7dacb96318991f8019ac
galwort <wthomasgorbett@gmail.com>
2024-12-31 16:31:36 -0500

    Add in audio

--- ui/src/app/pages/home/home.page.ts
+++ ui/src/app/pages/home/home.page.ts
@@ -9,16 +9,41 @@ export class HomePage implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    setTimeout(() => {
-      const overlay = document.getElementById('animation-overlay');
-      if (overlay) {
-        overlay.style.display = 'none';
+    const startButton = document.getElementById('start-button');
+    const audio = document.getElementById('logo-audio') as HTMLAudioElement;
+
+    startButton?.addEventListener('click', () => {
+      const startOverlay = document.getElementById('start-overlay');
+      if (startOverlay) {
+        startOverlay.style.display = 'none';
       }
 
-      const mainContent = document.getElementById('main-content');
-      if (mainContent) {
-        mainContent.style.display = 'block';
+      const animationOverlay = document.getElementById('animation-overlay');
+      if (animationOverlay) {
+        animationOverlay.style.display = 'flex';
       }
-    }, 4000);
+
+      if (audio) {
+        audio
+          .play()
+          .catch((error) => console.error('Error playing audio:', error));
+      }
+
+      setTimeout(() => {
+        if (animationOverlay) {
+          animationOverlay.style.display = 'none';
+        }
+
+        const mainContent = document.getElementById('main-content');
+        if (mainContent) {
+          mainContent.style.display = 'block';
+        }
+
+        if (audio) {
+          audio.pause();
+          audio.currentTime = 0;
+        }
+      }, 4000);
+    });
   }
 }
