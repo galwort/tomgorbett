7088e4f376d7e7fa6cf5add782ead6520c20000d
galwort <wthomasgorbett@gmail.com>
2025-01-02 13:44:06 -0500

    Add in placeholder content

--- ui/src/app/pages/home/home.page.ts
+++ ui/src/app/pages/home/home.page.ts
@@ -7,30 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
   activeLink: string = 'about';
-
   constructor() {}
-
   ngOnInit() {
     const randomBgHue = Math.floor(Math.random() * 360);
     let randomPrimaryHue = Math.floor(Math.random() * 360);
     while (randomPrimaryHue === randomBgHue) {
       randomPrimaryHue = Math.floor(Math.random() * 360);
     }
-
     const backgroundColor = this.hsvToHex(randomBgHue, 79, 15);
     const backgroundShade = this.hsvToHex(randomBgHue, 79, 30);
     const primaryColor = this.hsvToHex(randomPrimaryHue, 61, 100);
-
     const ionContent = document.querySelector('ion-content') as HTMLElement;
     if (ionContent) {
       ionContent.style.setProperty('--random-bg', backgroundColor);
       ionContent.style.setProperty('--random-bg-shade', backgroundShade);
       ionContent.style.setProperty('--random-primary', primaryColor);
     }
-
     const startButton = document.getElementById('start-button') as HTMLElement;
     const audio = document.getElementById('logo-audio') as HTMLAudioElement;
-
     startButton?.addEventListener('click', () => {
       const startOverlay = document.getElementById('start-overlay');
       if (startOverlay) {
@@ -49,7 +43,7 @@ export class HomePage implements OnInit {
         }
         const mainContent = document.getElementById('main-content');
         if (mainContent) {
-          mainContent.style.display = 'block';
+          mainContent.style.display = 'flex';
         }
         if (audio) {
           audio.pause();
@@ -58,20 +52,18 @@ export class HomePage implements OnInit {
       }, 4000);
     });
   }
-
   toggleActiveLink(section: string) {
     this.activeLink = section;
   }
-
   hsvToHex(h: number, s: number, v: number): string {
     s /= 100;
     v /= 100;
     const c = v * s;
     const x = c * (1 - Math.abs(((h / 60) % 2) - 1));
     const m = v - c;
-    let r = 0,
-      g = 0,
-      b = 0;
+    let r = 0;
+    let g = 0;
+    let b = 0;
     if (h >= 0 && h < 60) {
       r = c;
       g = x;
