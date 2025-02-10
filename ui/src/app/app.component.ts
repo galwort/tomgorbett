179bd174c2ada924b4670b99be6c23cd3ea5ffb0
galwort <wthomasgorbett@gmail.com>
2025-02-10 17:00:30 -0500

    Add IonApp and IonRouterOutlet to AppComponent imports

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { Router, Event as RouterEvent, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [IonicModule, RouterModule],
+  imports: [IonicModule, RouterModule, IonApp, IonRouterOutlet],
 })
 export class AppComponent {
   currentUrl: string = '';
