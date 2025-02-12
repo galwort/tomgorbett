1a52efe7e1e7073f5c09425bad45a7cc0ada45ef
galwort <wthomasgorbett@gmail.com>
2025-02-12 12:18:34 -0500

    Revert before angular upgrades

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+
+// Firebase imports
+import { initializeApp } from 'firebase/app';
 import { environment } from '../environments/environment';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideAuth, getAuth } from '@angular/fire/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+// Form and Charts modules
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgChartsModule } from 'ng2-charts';
 
@@ -17,14 +21,20 @@ import { NgChartsModule } from 'ng2-charts';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore()),
-    provideAuth(() => getAuth()),
     ReactiveFormsModule,
     NgChartsModule,
   ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {
+      provide: 'FIREBASE_APP',
+      useFactory: () => initializeApp(environment.firebase),
+    },
+    { provide: 'FIRESTORE', useFactory: () => getFirestore() },
+    { provide: 'AUTH', useFactory: () => getAuth() },
+  ],
   exports: [FormsModule, ReactiveFormsModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Fix for Ionic elements not recognized
 })
 export class AppModule {}
