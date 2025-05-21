953e438e3cf81fbd52123c87ba99aaf769082d64
galwort <wthomasgorbett@gmail.com>
2025-05-20 22:20:31 -0400

    Add functionality for daily component

--- ui/src/app/components/daily/daily.component.ts
+++ ui/src/app/components/daily/daily.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
+import { environment } from 'src/environments/environment';
+
+export const app = initializeApp(environment.firebase);
+export const db = getFirestore(app);
 
 @Component({
   selector: 'app-daily',
@@ -24,15 +30,38 @@ export class DailyComponent implements OnInit {
 
   ngOnInit() {}
 
+  private getFormattedDate(): string {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}${month}${day}`;
+  }
+
   async submitData() {
     this.isSubmitting = true;
     this.submitButtonText = 'Submitting...';
 
-    setTimeout(() => {
-      console.log('Form submitted:', this.dailyForm.value);
-      this.isSubmitting = false;
-      this.submitButtonText = 'Submit';
+    const mood = {
+      diet: this.moods.indexOf(this.dailyForm.value.diet),
+      marriage: this.moods.indexOf(this.dailyForm.value.marriage),
+      parents: this.moods.indexOf(this.dailyForm.value.parents),
+      productivity: this.moods.indexOf(this.dailyForm.value.productivity),
+      sleep: this.moods.indexOf(this.dailyForm.value.sleep),
+    };
+    const gratitude = this.dailyForm.value.gratitude;
+    const dateId = this.getFormattedDate();
+
+    try {
+      await setDoc(doc(db, 'mood', dateId), mood);
+      await setDoc(doc(db, 'gratitudes', dateId), { gratitude });
+      this.submitButtonText = 'Submitted';
       this.dailyForm.reset();
-    }, 2000);
+    } catch (error) {
+      this.submitButtonText = 'Submit Failed';
+    } finally {
+      this.isSubmitting = false;
+      setTimeout(() => (this.submitButtonText = 'Submit'), 2000);
+    }
   }
 }
