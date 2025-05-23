fafada5f4f7d91bd4f70ce52afe73f704a976b65
galwort <wthomasgorbett@gmail.com>
2025-05-23 15:27:28 -0400

    Add conditional rendering for analytics slides based on loaded state

--- ui/src/app/components/analytics/analytics.component.ts
+++ ui/src/app/components/analytics/analytics.component.ts
@@ -9,10 +9,16 @@ register();
   styleUrls: ['./analytics.component.scss'],
 })
 export class AnalyticsComponent implements AfterViewInit {
+  loadedSlides = [true, false, false];
+
   ngAfterViewInit() {
     setTimeout(() => {
       const el = document.querySelector('swiper-container.analytics-swiper') as any;
       el?.swiper?.update();
+      el?.addEventListener('swiperslidechange', () => {
+        const index = el.swiper?.activeIndex ?? 0;
+        this.loadedSlides[index] = true;
+      });
     }, 100);
   }
 }
